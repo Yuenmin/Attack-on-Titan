52180130a0340b726c5b28a7e797a4545ffbacfc
Yuenmin <74411878+Yuenmin@users.noreply.github.com>
2020-12-11 15:18:38 +0800

    Delete grass_ground_d.tsx

--- src/resources/grass_ground_d.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="2020.10.30" name="grass_ground_d" tilewidth="32" tileheight="32" tilecount="1024" columns="32">
- <image source="Textures/Terrain/grass_ground_d.jpg" trans="ffffff" width="1024" height="1024"/>
-</tileset>
