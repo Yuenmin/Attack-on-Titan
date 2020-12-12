97b75178d92dc18108e9d0f3037cb3d8e368b26f
Yuenmin <74411878+Yuenmin@users.noreply.github.com>
2020-12-13 01:38:57 +0800

    Update Main.java

--- src/game/Main.java
+++ src/game/Main.java
@@ -2,10 +2,12 @@ package game;
 
 import javafx.application.Application;
 import javafx.embed.swing.SwingNode;
-import javafx.scene.Group;
 import javafx.scene.Scene;
-import javafx.scene.paint.Color;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class Main extends Application {
 
@@ -14,24 +16,35 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws Exception {
 
-        Group root = new Group();
+        Pane root = new Pane();
         //Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-
-        Scene scene=new Scene(root, 1000, 800);
-        scene.setFill(Color.GREY);
+        
+        Scene scene=new Scene(root,1000,800);
         initMap();
         root.getChildren().add(swingNode);
 
         stage.setTitle("Attack On Titan");
+        stage.setMaximized(true);
         stage.setScene(scene);
         stage.show();
+        repaint();
     }
 
     public void initMap(){
+        new MapView(swingNode);
+        swingNode.setScaleX(0.59);
+        swingNode.setScaleY(0.55);
+        swingNode.setTranslateX(-540);
+        swingNode.setTranslateY(-330);
 
-        MapView mapView = new MapView();
-        mapView.jPanel(swingNode);
-        swingNode.setVisible(true);
+    }
+
+    public void repaint(){
+        new Timer().schedule(new TimerTask() {
+            public void run() {
+                swingNode.getContent().repaint();
+            }
+        }, 100L);
     }
 
     public static void main(String[] args) {
