0c1aca47d3c235dc49a7cc7fb2d8e745fdda81cb
CXiann <74451850+CXiann@users.noreply.github.com>
2020-12-14 18:22:21 +0800

    Add files via upload

---
+++ Attack-on-Titan-main/src/main/resources/com/attackontitan/male-leather-armor-set_1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="2020.10.30" name="male-leather-armor-set_1" tilewidth="48" tileheight="72" tilecount="96" columns="12">
+ <image source="Textures/Soldier/male-leather-armor-set_1.png" trans="ffffff" width="576" height="576"/>
+</tileset>
