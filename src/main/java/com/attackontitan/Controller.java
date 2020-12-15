0c366dff197b552a1ee2099ae38b500936244023
Yuenmin <74411878+Yuenmin@users.noreply.github.com>
2020-12-15 18:09:35 +0800

    Update Controller.java

--- src/main/java/com/attackontitan/Controller.java
+++ src/main/java/com/attackontitan/Controller.java
@@ -9,6 +9,7 @@ import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 import javafx.util.Duration;
@@ -55,18 +56,19 @@ public class Controller implements Initializable {
             swingNode.setScaleY(0.55);
             swingNode.setTranslateX(-540);
             swingNode.setTranslateY(-330);
-            Pane pane = new Pane();
+
+            AnchorPane pane=new AnchorPane();
             pane.getChildren().add(swingNode);
             Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
-            FadeTransition ft = new FadeTransition(Duration.millis(2500), pane);
+            FadeTransition ft = new FadeTransition(Duration.millis(2000), pane);
             ft.setFromValue(0.0);
             ft.setToValue(1.0);
-            ft.play();
             Scene scene = new Scene(pane);
             window.setScene(scene);
-            window.close();
+            window.hide();
             window.show();
             repaint();
+            ft.play();
         }
     }
 
@@ -75,7 +77,7 @@ public class Controller implements Initializable {
             public void run() {
                 swingNode.getContent().repaint();
             }
-        }, 200L);
+        }, 200);
     }
 
     public void initialize(URL url, ResourceBundle rb) {
