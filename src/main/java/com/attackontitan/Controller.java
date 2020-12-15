c2835674012d1a6e33efd7dc66b804d8341d224e
Yuenmin <74411878+Yuenmin@users.noreply.github.com>
2020-12-15 15:06:44 +0800

    Update Controller.java

--- src/main/java/com/attackontitan/Controller.java
+++ src/main/java/com/attackontitan/Controller.java
@@ -40,26 +40,34 @@ public class Controller implements Initializable {
         window.show();
     }
 
-    public void handleButtonAction2(ActionEvent event)  {
+    public void handleButtonAction2(ActionEvent event) throws IOException {
 
         String name = TF1.getText();
-        new MapView(swingNode);
-        swingNode.setScaleX(0.59);
-        swingNode.setScaleY(0.55);
-        swingNode.setTranslateX(-540);
-        swingNode.setTranslateY(-330);
-        Pane pane=new Pane();
-        pane.getChildren().add(swingNode);
-        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
-        FadeTransition ft = new FadeTransition(Duration.millis(2500),pane);
-        ft.setFromValue(0.0);
-        ft.setToValue(1.0);
-        ft.play();
-        Scene scene = new Scene(pane);
-        window.setScene(scene);
-        window.close();
-        window.show();
-        repaint();
+        if(name.isBlank()) {
+            Parent NAMEParent =FXMLLoader.load(this.getClass().getResource("NAME.fxml"));
+            Scene NAMEScene = new Scene(NAMEParent,App.getWidth(),App.getHeight());
+            Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+            window.setScene(NAMEScene);
+            window.show();
+        } else{
+            new MapView(swingNode);
+            swingNode.setScaleX(0.59);
+            swingNode.setScaleY(0.55);
+            swingNode.setTranslateX(-540);
+            swingNode.setTranslateY(-330);
+            Pane pane = new Pane();
+            pane.getChildren().add(swingNode);
+            Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            FadeTransition ft = new FadeTransition(Duration.millis(2500), pane);
+            ft.setFromValue(0.0);
+            ft.setToValue(1.0);
+            ft.play();
+            Scene scene = new Scene(pane);
+            window.setScene(scene);
+            window.close();
+            window.show();
+            repaint();
+        }
     }
 
     public void repaint(){
