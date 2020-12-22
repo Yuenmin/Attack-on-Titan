ed0cd1971df1179e6eed1bb1f73b5080b45a95b6
Yuenmin <74411878+Yuenmin@users.noreply.github.com>
2020-12-22 16:44:50 +0800

    Update App.java

--- src/main/java/com/attackontitan/App.java
+++ src/main/java/com/attackontitan/App.java
@@ -5,6 +5,7 @@ import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Group;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
@@ -18,16 +19,18 @@ import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 
+import java.util.TimerTask;
+
 public class App extends Application {
 
     private static double height;
     private static double width;
     private static Stage pStage;
-    Group group=new Group();
-    Pane pane=new Pane();
-    Scene scene;
-
-    public App(){}
+    private Group group=new Group();
+    private Group column =new Group();
+    private Group number=new Group();
+    private static Pane pane=new Pane();
+    private Scene scene;
 
     @Override
     public void start(Stage primaryStage) throws Exception {
@@ -43,8 +46,6 @@ public class App extends Application {
         primaryStage.setTitle("Attack On Titan");
         primaryStage.getIcons().add(new Image("com/attackontitan/icon.png"));
         primaryStage.setScene(scene);
-        primaryStage.setMaxHeight(800);
-        primaryStage.setMaxWidth(1350);
         primaryStage.setResizable(false);
         primaryStage.show();
         height=scene.getHeight();
@@ -61,14 +62,12 @@ public class App extends Application {
         tower.setY(0);
         tower.setFitWidth(App.getWidth());
         tower.setFitHeight(App.getHeight());
+        FadeTransition ft=setFadeTransition(bg,2000);
         group.getChildren().add(bg);
-        FadeTransition ft = new FadeTransition(Duration.millis(2000),bg);
-        ft.setFromValue(0.0);
-        ft.setToValue(1.0);
         scene = new Scene(group,getWidth(),getHeight());
         Stage stage=App.getPrimaryStage();
         stage.setScene(scene);
-        ft.play();
+        stage.setResizable(false);
         ft.setOnFinished(actionEvent -> {
             group.getChildren().addAll(
                     column,
@@ -78,18 +77,29 @@ public class App extends Application {
                     new Cannon(0).cannonGroup,
                     new Soldier().soldierGroup
             );
+            drawNum();
+            Platform.runLater(new TimerTask() {
+                @Override
+                public void run() {
+                    drawColumn();
+                    setFadeTransition(column,2000);
+                }
+            });
         });
     }
 
-    static Group column =new Group();
-    static Group number=new Group();
-    public static void drawColumn(){
-        for (int i =590; i>20; i-=65) {
+    public void drawColumn() {
+        Group column = getColumn();
+        for (int i = 590; i > 20; i -= 65) {
             Line line = new Line(0, i, getWidth(), i);
             line.setStroke(Color.GRAY);
             line.setStrokeWidth(1.5);
             column.getChildren().add(line);
         }
+    }
+
+    public void drawNum(){
+        Group number = getNumber();
         int y=635;
         for(int i=0;i<=9;i++){
             Text num=new Text(30,y,Integer.toString(i));
@@ -99,17 +109,51 @@ public class App extends Application {
         }
     }
 
-    public void newATitan(double x,double y){ pane.getChildren().add(new ATitan(x,y).aTitan); }
+    public void spawnATitan(double x, double y){
+        Pane pane=getPane();
+        pane.getChildren().add(new ATitan(x,y).getATitan());
+    }
 
-    public void newCTitan(double x){ pane.getChildren().add(new CTitan(x).cTitan); }
+    public void spawnCTitan(int x){
+        Pane pane=getPane();
+        pane.getChildren().add(new CTitan(x).getCTitan());
+    }
 
-    public static Stage getPrimaryStage() { return pStage; }
+    public FadeTransition setFadeTransition(Node node,double duration){
+        FadeTransition ft = new FadeTransition(Duration.millis(duration),node);
+        ft.setFromValue(0.0);
+        ft.setToValue(1.0);
+        ft.play();
+        return ft;
+    }
 
-    public static double getHeight(){ return height; }
+    public Pane getPane() {
+        return pane;
+    }
 
-    public static double getWidth(){ return width; }
+    public Group getColumn() {
+        return column;
+    }
 
-    public static void main(String[] args) { launch(args); }
+    public Group getNumber() {
+        return number;
+    }
+
+    public static Stage getPrimaryStage() {
+        return pStage;
+    }
+
+    public static double getHeight(){
+        return height;
+    }
+
+    public static double getWidth(){
+        return width;
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
 
 
