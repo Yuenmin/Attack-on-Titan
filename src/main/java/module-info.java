2862263510064ad6fa465d89bc58dec5d20fc929
Yuenmin <u2005375@siswa.um.edu.my>
2020-12-30 12:29:09 +0800

    Remove folder

--- src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module AttackOnTitan {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires javafx.swing;
-    requires libtiled;
-    requires java.xml.bind;
-
-    opens com.attackontitan;
-}
